3c2b0598bff9865e92f209245e4f184c1eb75637
dohan.kim <junyeob.kim@onch3.co.kr>
2026-04-13 16:16:34 +0900

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* 빌드 시 정적 HTML 파일들을 생성하도록 설정 (GitHub Pages 필수) */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/dohan-portfolio',
 };
 
 export default nextConfig;
