6f4528ef5411db412da4c30a440322f58cab4f6e
dohan.kim <junyeob.kim@onch3.co.kr>
2026-04-17 13:04:18 +0900

    test remove

--- components/AIChatBot.tsx
+++ components/AIChatBot.tsx
@@ -90,7 +90,7 @@ const AIChatBot = () => {
     setMessages(prev => [...prev, { role: 'user', text: userMsg }]);
     setInput('');
     setIsLoading(true);
-    console.log('process.env.NEXT_PUBLIC_GROQ_API_KEY', process.env.NEXT_PUBLIC_GROQ_API_KEY);
+
     const groq = new Groq({
       apiKey: process.env.NEXT_PUBLIC_GROQ_API_KEY,
       dangerouslyAllowBrowser: true
